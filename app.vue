782b19f5b668cfa47b1b8b7b321262ab236dda97
sailesh <sailesh@jobins.jp>
2024-01-03 16:33:50 +0545

    feat: added language translation

--- app.vue
+++ app.vue
@@ -1,4 +1,21 @@
+<script setup>
+const {availableLocales} = useI18n()
+
+</script>
+
 <template>
-  <div>{{ $t('hello') }}
+  <div>
+
+    <form class="flex flex-col">
+      <NuxtLink
+        v-for="item in availableLocales"
+        :key="item"
+        :to="`/`+(item ==='en'?'':item)"
+      >
+        {{ item }}
+      </NuxtLink>
+
+      <p>{{ $t('hello') }}</p>
+    </form>
   </div>
 </template>
--- i18n.config.ts
+++ i18n.config.ts
@@ -1,11 +1,37 @@
 import en from './locales/en.json'
 import jp from './locales/jp.json'
 
-export default defineI18nConfig(() => ({
-  legacy: false,
-  locale: 'en',
-  messages: {
+const availableLocales = {
     en: en,
-    jp:jp
-  }
+    jp: jp
+}
+export default defineI18nConfig(() => ({
+    legacy: false,
+    messages: {
+        en: en,
+        jp: jp
+    },
+    locales: [
+        {
+            code: "en",
+            name: "English",
+            iso: 'en-US',
+            file: 'en.js'
+        },
+        {
+            code: "jp",
+            name:"Japan",
+            iso: 'jp-JP',
+            file: 'jp.json'
+        }
+    ],
+    defaultLocale: 'en',
+    lazy: true,
+    strategy: 'prefix_except_default',
+    langDir: 'locales',
+    vueI18n: {
+        fallbackLocale: "en",
+        messages: availableLocales
+    }
+
 }))
